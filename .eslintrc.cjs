346fddf360d6a271e4f82265bdfabc1c95293315
imnvk <kevin.nvk.nunes@gmail.com>
2023-03-10 21:45:04 +0100

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'no-console': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+};
